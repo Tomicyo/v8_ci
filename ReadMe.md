06f6603914c7e3bed8561d48d3b4e4b03a04fa9f
Tomicyo <dsotsen@gmail.com>
2017-09-26 22:56:53 +0800

    build linux

--- ReadMe.md
+++ ReadMe.md
@@ -1,3 +1,4 @@
 # Google's V8 CI
 
 [![](https://ci.appveyor.com/api/projects/status/isnqyok0176y3kx3?svg=true)](https://ci.appveyor.com/project/Tomicyo/v8-ci)
+[![Build Status](https://travis-ci.org/Tomicyo/v8_ci.svg?branch=master)](https://travis-ci.org/Tomicyo/v8_ci)
---
+++ v8_build.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/bash
+DEPOT_DIR=`pwd`/depot_tools
+git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git $DEPOT_DIR
+export PATH=$PATH:$DEPOT_DIR:`pwd`/v8/buildtools/linux64
+fetch v8
+gclient sync
+cd v8
+mkdir -p out.gn/x64.release
+cp ../args.gn out.gn/x64.release
+gn gen out.gn/x64.release
+ninja -C out.gn/x64.release v8
+mkdir -p artifacts/lib/linux64r
+mkdir -p artifacts/bin
+cp out.gn/x64.release/*.so artifacts/lib/linux64r
+cp out.gn/x64.release/*.bin artifacts/bin
+cp out.gn/x64.release/*.dat artifacts/bin
+cp -r include artifacts/
+cd artifacts
+zip -r v8_linux.zip .
