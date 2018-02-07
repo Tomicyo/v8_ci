29b0345b52e41a6e3eb9823c67f8023bca6c4cc7
alexqzhou <alexqzhou@tencent.com>
2018-02-07 16:22:35 +0800

    update v8 ci

--- v8_ci.bat
+++ v8_ci.bat
@@ -1,5 +1,6 @@
 @echo off
 set DEPOT_DIR=%~dp0\..\depot_tools
+set PATH=%DEPOT_DIR%;%PATH%
 if not exist %DEPOT_DIR% goto GetDepot
 goto GetV8
 :GetDepot
@@ -23,24 +24,25 @@ call v8_gen;
 
 cd v8
 ninja -C out.gn/x64.release v8
+@rem BuildConsole.exe /command="ninja.exe -C out.gn/x64.release v8" /profile="E:\bin\Xoreax\IncrediBuild\Profiles\chromium.ib_profile.xml"
 
 :Install
 mkdir output
 mkdir output\include
-xcopy /S include output\include
+xcopy /S /Y include output\include
 echo "=========== Include Copy Finished =============="
 mkdir output\bin
 mkdir output\bin\win64_vc150r
-xcopy /S out.gn\x64.release\*.dat output\bin\win64_vc150r
-xcopy /S out.gn\x64.release\*.bin output\bin\win64_vc150r
-xcopy /S out.gn\x64.release\v8*.dll output\bin\win64_vc150r
-xcopy /S out.gn\x64.release\v8*.pdb output\bin\win64_vc150r
-xcopy /S out.gn\x64.release\icu*.dll output\bin\win64_vc150r
-xcopy /S out.gn\x64.release\icu*.pdb output\bin\win64_vc150r
+xcopy /S /Y out.gn\x64.release\*.dat output\bin\win64_vc150r
+xcopy /S /Y out.gn\x64.release\*.bin output\bin\win64_vc150r
+xcopy /S /Y out.gn\x64.release\v8*.dll output\bin\win64_vc150r
+xcopy /S /Y out.gn\x64.release\v8*.pdb output\bin\win64_vc150r
+xcopy /S /Y out.gn\x64.release\icu*.dll output\bin\win64_vc150r
+xcopy /S /Y out.gn\x64.release\icu*.pdb output\bin\win64_vc150r
 echo "=========== Bin Copy Finished =============="
 mkdir output\lib\
 mkdir output\lib\win64_vc150r
-xcopy /S out.gn\x64.release\*.lib output\lib\win64_vc150r
+xcopy /S /Y out.gn\x64.release\*.lib output\lib\win64_vc150r
 echo "=========== Lib Copy Finished =============="
 :End
 echo Ending
